e9371216079a38a44671b445e0e05a59be35b79a
Soumen Ghosh <soumeng007@gmail.com>
2018-01-27 11:41:01 +0600

    Initial Commit

---
+++ Quiz1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz1
+//
+//  Created by SOUMEN GHOSH on 27/1/18.
+//  Copyright © 2018 Sg106. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
