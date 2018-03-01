4a504c1a97298e5c3223b65c61e8051221ef1717
Soumen Ghosh <soumeng007@gmail.com>
2018-03-01 20:14:16 +0600

    final commit

---
+++ Quiz1/Controller/CollectionController.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionController.swift
+//  Quiz1
+//
+//  Created by SOUMEN GHOSH on 27/1/18.
+//  Copyright © 2018 Sg106. All rights reserved.
+//
+
+import Foundation
---
+++ Quiz1/View/ShoeCell.swift
@@ -0,0 +1,9 @@
+//
+//  ShoeCell.swift
+//  Quiz1
+//
+//  Created by SOUMEN GHOSH on 27/1/18.
+//  Copyright © 2018 Sg106. All rights reserved.
+//
+
+import Foundation
